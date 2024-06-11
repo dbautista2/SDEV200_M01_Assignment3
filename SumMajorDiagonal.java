1882a5bf90138f61efe5307bc0beec96370cecce
dbautista2 <108000972+dbautista2@users.noreply.github.com>
2024-06-11 15:31:49 -0400

    Add files via upload

---
+++ SumMajorDiagonal.java
@@ -0,0 +1,31 @@
+// 8.29
+import java.util.Scanner;
+
+public class SumMajorDiagonal {
+    public static double SumMajorDiagonal(double[][] m) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+        return sum;
+    }
+
+    public static void main(String[]args ) {
+        Scanner input = new Scanner(System.in);
+
+        double [][] matrix = new double[4][4];
+
+        System.out.println("Enter a 4 by 4 matrix row by row:");
+        for (int i = 0; i < 4; i++ ) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = input.nextDouble();
+            }
+        }
+
+        double sum = SumMajorDiagonal(matrix);
+
+        System.out.println("Sum of the elements in the major diagonal is " + sum);
+
+        input.close();
+    }
+}
